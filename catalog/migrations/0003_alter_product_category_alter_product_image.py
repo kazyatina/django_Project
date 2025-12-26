9779fb68c8aae43c7ee65f5104d6b1904312cda1
olga <koa0308@gmail.com>
2025-12-26 23:39:56 +0400

    обновление миграций

--- catalog/migrations/0003_alter_product_category_alter_product_image.py
+++ catalog/migrations/0003_alter_product_category_alter_product_image.py
@@ -7,18 +7,24 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('catalog', '0002_rename_about_category_category_description_and_more'),
+        ("catalog", "0002_rename_about_category_category_description_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='product',
-            name='category',
-            field=models.ForeignKey(choices=[('test', 'Тестовая'), ('main', 'Основная')], on_delete=django.db.models.deletion.CASCADE, to='catalog.category'),
+            model_name="product",
+            name="category",
+            field=models.ForeignKey(
+                choices=[("test", "Тестовая"), ("main", "Основная")],
+                on_delete=django.db.models.deletion.CASCADE,
+                to="catalog.category",
+            ),
         ),
         migrations.AlterField(
-            model_name='product',
-            name='image',
-            field=models.ImageField(blank=True, null=True, upload_to='images/', verbose_name='Изображение'),
+            model_name="product",
+            name="image",
+            field=models.ImageField(
+                blank=True, null=True, upload_to="image/", verbose_name="Изображение"
+            ),
         ),
     ]
