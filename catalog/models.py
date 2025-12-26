a4c82fa15f1034e236d00536dcf86482057fb1d9
olga <koa0308@gmail.com>
2025-12-26 23:43:34 +0400

    обновление

--- catalog/models.py
+++ catalog/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-from django.db import models
-
-'''Product:
+"""Product:
 наименование,
 описание,
 изображение,
@@ -14,57 +12,60 @@ from django.db import models
 Category:
 наименование,
 описание.
-'''
+"""
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Название', unique=True)
-    description = models.CharField(max_length=150, verbose_name='Описание')
+    name = models.CharField(max_length=150, verbose_name="Название", unique=True)
+    description = models.CharField(max_length=150, verbose_name="Описание")
 
     def __str__(self):
         """Определяет строковое представление объекта"""
-        return f'{self.name}, {self.description}'
+        return f"{self.name}, {self.description}"
 
-    def Meta(self):
+    class Meta:
         """Используется для добавления метаданных к модели. Он определяет такие свойства, как порядок сортировки,
         наименование модели в единственном и множественном числе и другие"""
         # verbose_name определяют отображаемое имя модели в единственном и множественном числе
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        # ordering определяет порядок сортировки объектов при выборке из базы данных
-        ordering = ['name']
-        # db_table указывает имя таблицы в базе данных, к которой привязана модель
-        db_table = 'django_project'
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
 
 class Product(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Название', unique=True) # поле для хранения строк
-    description = models.TextField(verbose_name='Описание', null=True, blank=True) # поле для хранения больших текстов
-    image = models.ImageField(verbose_name='Изображение', upload_to='images/',null=True, blank=True) # поле для хранения информации о загруженных изображениях
-    price = models.IntegerField(verbose_name='Цена за покупку') # поле для хранения целых чисел
-    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
-    updated_at = models.DateField(verbose_name='Дата последнего изменения', auto_now=True)
+    name = models.CharField(
+        max_length=150, verbose_name="Название", unique=True
+    )  # поле для хранения строк
+    description = models.TextField(
+        verbose_name="Описание", null=True, blank=True
+    )  # поле для хранения больших текстов
+    image = models.ImageField(
+        verbose_name="Изображение", upload_to="product/image", null=True, blank=True
+    )  # поле для хранения информации о загруженных изображениях
+    price = models.IntegerField(
+        verbose_name="Цена за покупку"
+    )  # поле для хранения целых чисел
+    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    updated_at = models.DateField(
+        verbose_name="Дата последнего изменения", auto_now=True
+    )
 
-    FOOD = 'food'
-    CHEMICAL = 'chemical'
-    PETS = 'pet'
-    LIST_CHOOSES = [
-        (FOOD, 'еда'),
-        (CHEMICAL, 'бытовая химия'),
-        (PETS, 'для питомцев'),
-    ]
-    category = models.ForeignKey(Category, choices=LIST_CHOOSES, on_delete=models.CASCADE) # поле для создания внешнего ключа на другую модель
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products",
+    )  # поле для создания внешнего ключа на другую модель
 
     def __str__(self):
         """Определяет строковое представление объекта"""
-        return f'{self.name} - {self.price}, {self.description}'
+        return f"{self.name} - {self.price}, {self.description}, {self.category}"
 
-    def Meta(self):
+    class Meta:
         """Используется для добавления метаданных к модели. Он определяет такие свойства, как порядок сортировки,
         наименование модели в единственном и множественном числе и другие"""
         # verbose_name определяют отображаемое имя модели в единственном и множественном числе
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
         # ordering определяет порядок сортировки объектов при выборке из базы данных
-        ordering = ['name']
+        ordering = ["name"]
         # db_table указывает имя таблицы в базе данных, к которой привязана модель
-        db_table = 'django_project'
+        # db_table = "django_project"
--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -23,68 +24,70 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.getenv('DEBUG') == 'True' else False  # это дописать, чтобы работало, х.з что и зачем
+DEBUG = (
+    True if os.getenv("DEBUG") == "True" else False
+)  # это дописать, чтобы работало, х.з что и зачем
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # 'students',
-    'catalog',
+    "catalog",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('password'),
-        'HOST': os.getenv('host'),
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("NAME"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("password"),
+        "HOST": os.getenv("host"),
+        "PORT": "5432",
     }
 }
 
@@ -94,16 +97,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -111,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -123,11 +126,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'uploads')
+# MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = 'media/product/image/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
